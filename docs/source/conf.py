3d6a46982231d6f5b938ce4ba1914b2cb5de5156
Erik Quaeghebeur <epyglpki@equaeghe.nospammail.net>
2015-10-14 22:28:51 +0200

    update python version referenced in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -268,7 +268,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/3.3/': None}
+intersphinx_mapping = {'http://docs.python.org/3.4/': None}
 
 # Substitutions
 rst_prolog = """
